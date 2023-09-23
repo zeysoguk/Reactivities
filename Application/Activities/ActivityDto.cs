bd2a8ef300d8bcd15aaa6fb00bfb1e0ca81944d4
Zeynep Kardelen Soğukoğlu <64661378+zeysoguk@users.noreply.github.com>
2023-09-23 20:49:10 +0300

    Entity Framework Relationships

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
+using Application.Profiles;
 
-namespace Domain
+namespace Application.Activities
 {
-    public class Activity
+    public class ActivityDto
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
@@ -11,7 +12,8 @@ namespace Domain
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        public string HostUsername { get; set; }
         public bool IsCancelled { get; set; }
-        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
+        public ICollection<Profile> Attendees { get; set; }
     }
 }
